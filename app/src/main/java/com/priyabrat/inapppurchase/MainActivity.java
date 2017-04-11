602613a3f04d09e8ed9b5bee8aee1458cc0a4a91
PriyabratP <ppadhybam@gmail.com>
2017-04-11 15:19:36 +0530

    Initial commit

---
+++ app/src/main/java/com/priyabrat/inapppurchase/MainActivity.java
@@ -0,0 +1,15 @@
+package com.priyabrat.inapppurchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //https://github.com/anjlab/android-inapp-billing-v3?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=1399
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
